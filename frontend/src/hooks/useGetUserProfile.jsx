eabf2169a60db55d6e6969d5be0f82bd189325ea
krush7789 <135722989+krush7789@users.noreply.github.com>
2025-08-26 00:26:56 +0530

    production ready 5

--- frontend/src/hooks/useGetUserProfile.jsx
+++ frontend/src/hooks/useGetUserProfile.jsx
@@ -10,7 +10,7 @@ const useGetUserProfile = (userId) => {
     useEffect(() => {
         const fetchUserProfile = async () => {
             try {
-                const res = await axios.get(`http://localhost:3000/api/v1/user/${userId}/profile`, { withCredentials: true });
+                const res = await axios.get(`https://let-s-talk-lq7h.onrender.com/api/v1/user/${userId}/profile`, { withCredentials: true });
                 if (res.data.success) { 
                     dispatch(setUserProfile(res.data.user));
                 }
